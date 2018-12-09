91bc3ce88ea72640bd1f646cdfb72634fdf63a55
Achudh Krishna <achudhk@gmail.com>
2018-12-09 10:49:48 +0530

    added ethereum

---
+++ src/app/ethcontract.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import * as Web3 from ‘web3’;
+import * as TruffleContract from ‘truffle-contract’;
+declare let require: any;
+declare let window: any;
+@Injectable({
+  providedIn: 'root'
+})
+export class EthcontractService {
+
+  constructor() { }
+}
