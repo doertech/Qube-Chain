2844fc52198a50db93c567139eaabc99cf9dd0eb
Achudh Krishna <achudhk@gmail.com>
2018-12-09 12:18:44 +0530

    added more components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
+import { SignupComponent } from './signup/signup.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { NavComponent } from './nav/nav.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
-  { path: 'login', component: LoginComponent}
+  { path: 'login', component: LoginComponent},
+  { path: 'aboutus', component: AboutusComponent},
+  { path: 'contactus', component: ContactusComponent},
+  { path: 'nav', component: NavComponent},
+  { path: 'signup', component: SignupComponent}
 ];
 
 @NgModule({
